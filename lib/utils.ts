a6e380aba3369333dcecd91fe38c36eab1cf24cc
Regényi Mátyás <regenyimatyas@Mac.home>
2026-04-12 09:09:11 +0200

    feat: add formatPrice utility for Hungarian Ft formatting

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatPrice(n: number): string {
+  return `${new Intl.NumberFormat("hu-HU").format(n)} Ft`
+}
